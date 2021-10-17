1377ea2118f5ecb9bb2efa9cd0319b5ea6cb411e
Jose Rios <joseriosdev@gmail.com>
2021-10-16 19:46:15 -0500

    Project #2 done -> Functions

--- 1_JS-fundamentals/2_functions/Project/app.js
+++ 1_JS-fundamentals/2_functions/Project/app.js
@@ -15,3 +15,88 @@ You should test all the functions in the browser
 Create an app that uses one of the prev functions taking the params from user input (prompt)
 */
 
+// #1
+function aritmeticOperations(a, b) {
+	return {
+		sum: a + b,
+		rest: a - b,
+		multiplication: a * b,
+		division: a / b
+	}
+}
+
+// #2
+var randomInRange = function(min, max) {
+	return Math.round(Math.random() * (max - min) + min);
+}
+
+// #3
+var getBasicMathsMethods = decimalNumber => {
+	return {
+		ceil: parseFloat( Math.ceil(decimalNumber).toFixed(2) ),
+		floor: parseFloat( Math.floor(decimalNumber).toFixed(2) ),
+		round: parseFloat( Math.round(decimalNumber).toFixed(2) )
+	}
+}
+
+// #4
+function circleArea(radius) {
+	console.log(`The area of the circle with radius ${radius} is: ${(2 * Math.PI * radius).toFixed(2)}`);
+}
+
+// #5
+var printRandomTexts = function() {
+	var randomTextsList = [
+		'Olá',
+		'Hola',
+		'Hello',
+		'Hallo',
+		'Ciao',
+		'Salut',
+		'Привет',
+		'こんにちは',
+		'你好呀',
+		'안녕하세요'
+	]
+	var getRandomText1 = randomTextsList[randomInRange(0, randomTextsList.length-1)];
+	var getRandomText2 = randomTextsList[randomInRange(0, randomTextsList.length-1)];
+	var getRandomText3 = randomTextsList[randomInRange(0, randomTextsList.length-1)];
+
+	console.log(getRandomText1);
+	alert(getRandomText2);
+	document.write(getRandomText3);
+}
+
+// #6
+var milesToKM = miles => {
+	var km = (miles * 1.6).toFixed(2);
+	console.log(`The miles ${miles} to KM will be ${km}`);
+	return km;
+}
+
+// #7
+function countdown(initialNumber) {
+	setInterval(() => {
+		console.log(initialNumber--);
+	}, 1000);
+}
+
+// #8
+var lastIsNull = function(arr) {
+	arr.pop();
+	arr.push(null);
+	return arr;
+}
+
+// #9
+var tripHoursCalculator = (speed, distance) => (distance / speed).toFixed(1);
+
+
+// Here's the actual app
+alert('RANDOM NUMBER BETWEEN RANGE!');
+
+var min = parseInt(prompt('Min value please'));
+var max = parseInt(prompt('Max value please'));
+var pick = randomInRange(min, max);
+
+alert('This is my pick => ' + pick);
